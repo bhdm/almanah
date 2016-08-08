ffa707d5bc9b18e0e11af1cb2953001e517451e0
bhdm <bhd.m@ya.ru>
2016-08-09 00:14:00 +0300

    u

---
+++ src/AdminBundle/Command/RemoveEmailCommand.php
@@ -0,0 +1,31 @@
+<?php
+namespace AdminBundle\Command;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class RemoveEmailCommand extends ContainerAwareCommand
+{
+
+    protected function configure()
+    {
+        $this->setName('remove:mail');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+
+
+        $em           = $this->getContainer()->get('doctrine')->getManager();
+        $pdo          = $em->getConnection();
+
+        include_once 'emails.php';
+
+        foreach ($emails as $mail){
+            $updateDoctor   = $pdo->prepare('DELETE FROM email WHERE email="'.$mail.'"');
+            $updateDoctor->execute();
+        }
+    }
+}
