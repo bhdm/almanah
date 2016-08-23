9a602717544e74ddf4cfb5b072e96276fc562f63
bhdm <bhd.m@ya.ru>
2016-08-23 23:38:15 +0300

    add image upload for ckeditor

--- src/AdminBundle/Resources/public/ckeditor/config.js
+++ src/AdminBundle/Resources/public/ckeditor/config.js
@@ -7,7 +7,7 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
-
+    config.filebrowserUploadUrl = '../../ckupload.php';
     config.toolbarGroups = [
 //        { name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
 //        { name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
